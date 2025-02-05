0ff4680bbe8939654d9f23fb3c856e3883ec33f6
lxy <577086410@qq.com>
2025-02-05 22:39:01 +0800

    Feat: avg time and effitiency of 1000 runs.

--- matmul_2gpu.py
+++ matmul_2gpu.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 from jax.sharding import PartitionSpec as P, NamedSharding
+import time
 
 # 使用 jax.jit 优化编译
 # @jax.jit
@@ -17,19 +18,32 @@ def parallel_matmul(matrix1, matrix2):
 key = jax.random.PRNGKey(0) #  添加 PRNGKey
 key1, key2 = jax.random.split(key) # 使用不同的随机数序列
 
-matrix1 = jax.random.normal(key1, (1024, 1024)) 
-matrix2 = jax.random.normal(key2, (1024, 1024)) 
+matrix1 = jax.random.normal(key1, (8192, 8191), dtype=jnp.float32).block_until_ready()
+matrix2 = jax.random.normal(key2, (8191, 8190), dtype=jnp.float32).block_until_ready()
+print ("matrix1: ", matrix1.shape)
+print ("matrix2: ", matrix2.shape)
 
 mesh = jax.make_mesh((2, 1), ('x', 'y'))
 matrix1_sharding = NamedSharding(mesh, P('x', None))
 matrix2_sharding = NamedSharding(mesh, P())
 
-matrix1_d = jax.device_put(matrix1, matrix1_sharding)
-matrix2_d = jax.device_put(matrix2, matrix2_sharding)
+matrix1_d = jax.device_put(matrix1, matrix1_sharding).block_until_ready()
+matrix2_d = jax.device_put(matrix2, matrix2_sharding).block_until_ready()
 
-# 调用并行化的模型
-output_d = parallel_matmul(matrix1_d, matrix2_d)
+tick0 = time.time()
+for __ in range(1000):
+  output_d = parallel_matmul(matrix1_d, matrix2_d)
+output_d.block_until_ready()
+tick1 = time.time()
+multi_time = (tick1 - tick0)
+print("Multi GPU Time: ", multi_time, 'ms')
 
-output_h = parallel_matmul(matrix1, matrix2)
-
+tick0 = time.time()
+for __ in range(1000):
+  output_h = parallel_matmul(matrix1, matrix2)
+output_h.block_until_ready()
+tick1 = time.time()
+single_time = (tick1 - tick0)
+print("Single GPU Time: ", single_time, 'ms')
+print("Parallel efficiency: ", single_time / multi_time / 2 * 100 , '%')
 assert jnp.allclose(output_d, output_h, atol=1e-3)
